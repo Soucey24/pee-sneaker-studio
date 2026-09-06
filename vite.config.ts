fb484b31c791012721282a3f1ef13a38cb599668
RockZ37 <delalirock5@gmail.com>
2026-09-06 05:14:25 +0000

    metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
@@ -7,6 +8,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    tailwindcss(),
     react(),
   ],
   resolve: {
